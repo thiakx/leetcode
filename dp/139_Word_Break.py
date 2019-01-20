40fce4252ab85875bbf1d476e8ab884b0c40e954
Thia Kai Xin <kaixin@techinasia.com>
2019-01-20 20:15:52 +0800

    Solve 139. Word Break

--- dp/139_Word_Break.py
+++ dp/139_Word_Break.py
@@ -1,6 +1,7 @@
 import unittest
 
-# substring and subseq
+# Given a non-empty string s and a dictionary wordDict containing a list of non-empty words,
+# determine if s can be segmented into a space-separated sequence of one or more dictionary words.
 
 s = "leetcode"
 wordDict = ["leet", "code"]
