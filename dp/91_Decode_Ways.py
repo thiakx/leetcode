219ca125e2941bb2308b44b3882cc2af5f6b5036
Thia Kai Xin <kaixin@techinasia.com>
2019-01-20 22:30:37 +0800

    Solve 91. Decode Ways

--- dp/91_Decode_Ways.py
+++ dp/91_Decode_Ways.py
@@ -34,7 +34,7 @@ class Solution:
         for i in range(1, n + 1):
             if s[i - 1] != "0":  # 0 has no matching alphabet
                 dp[i] += dp[i - 1]
-            if i != 1 and s[i - 2:i] < "27" and s[i - 2:i] > "09":
+            if i != 1 and "09" < s[i - 2:i] < "27":
                 dp[i] += dp[i - 2]
         return dp[-1]
 
