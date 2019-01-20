13a9ef88c5972e802eea17ce891cd390e427ea94
Thia Kai Xin <kaixin@techinasia.com>
2019-01-20 20:15:29 +0800

    Solve 139. Word Break

---
+++ dp/139_Word_Break.py
@@ -0,0 +1,33 @@
+import unittest
+
+# substring and subseq
+
+s = "leetcode"
+wordDict = ["leet", "code"]
+output_value = True
+
+
+class funcTest(unittest.TestCase):
+    def test(self):
+        solution = Solution()
+        self.assertEqual(solution.wordBreak(s, wordDict), output_value)
+
+
+class Solution:
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        n = len(s)
+        dp = n * [False]
+        for i in range(n):
+            for w in wordDict:
+                if w == s[i - len(w) + 1:i + 1] and (dp[i - len(w)] or i - len(w) == -1):
+                    dp[i] = True
+        return dp[-1]
+
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)  # extra conditions for jupyter notebook
