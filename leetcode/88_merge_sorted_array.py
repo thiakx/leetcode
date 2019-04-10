bffabd0b35755a803d22dd2ec754fe25e08ad36e
Thia Kai Xin <kaixin@techinasia.com>
2019-04-10 15:22:24 +0800

    merge sorted array done

--- leetcode/88_merge_sorted_array.py
+++ leetcode/88_merge_sorted_array.py
@@ -1,6 +1,7 @@
 import unittest
 from typing import List
 
+
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> List[int]:
         """
@@ -11,7 +12,7 @@ class Solution:
         # edge cases
         # num1 empty, return num2
         if m == 0:
-            nums1 = nums2
+            nums1[:] = nums2[:]  # python trick to copy
         # num2 empty, do nothing, return num1
         elif n == 0:
             pass
@@ -35,12 +36,7 @@ class Solution:
                 pointer1_end -= 1
 
             # if there are still left overs from num2, we fill forward. if num1, we do nothing, done.
-            while pointer2 >= 0:
-                nums2_temp = nums2[pointer2]
-                nums1[pointer1_end] = nums2_temp
-                nums2[pointer2] = 0
-                pointer2 -= 1
-                pointer1_end -= 1
+            nums1[0:pointer2 + 1] = nums2[0:pointer2 + 1]
 
         return nums1
 
