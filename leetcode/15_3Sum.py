494c00950a4625f4a6a66b7868a6305cc334f0d2
Thia Kai Xin <kaixin@techinasia.com>
2019-04-12 17:30:14 +0800

    3sum done

---
+++ leetcode/15_3Sum.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    # reference: leetcode forums
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) < 3:
+            return []
+        res = []
+        nums.sort()
+        # need at least 3 numbers to continue.
+        for i in range(len(nums) - 2):
+            # when i = 0, dont check if it's duplicated.
+            # nums[i] == nums[i-1] is to prevent checking duplicate again.
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                s = nums[i] + nums[left] + nums[right]
+                if s < 0:
+                    left += 1
+                elif s > 0:
+                    right -= 1
+                else:
+                    res.append((nums[i], nums[left], nums[right]))
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+        return res
